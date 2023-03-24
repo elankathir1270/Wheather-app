1f5b8b09d7d504efbf17351d9a5eaa12df57b279
elankathir1270 <elankathirskr92@gmail.com>
2023-03-24 23:20:53 +0530

    established secure API endpoints

--- src/services/wheatherAPI.js
+++ src/services/wheatherAPI.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 
 export const wheatherApi = axios.create({
-    baseURL: "http://api.weatherapi.com/v1",
+    baseURL: "https://api.weatherapi.com/v1",
     params: { key: "a13086e921ec4885a3d72648232403"}
   })
